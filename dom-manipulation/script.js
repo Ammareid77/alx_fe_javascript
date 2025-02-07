90104e137734dde9e4d5cdefd3943a5591258915
Ammar Eid <ammareidd7@gmail.com>
2025-02-07 14:51:23 +0200

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -112,6 +112,16 @@ async function postQuoteToServer(newQuote) {
       },
       body: JSON.stringify(Blob), // Convert newQuote to JSON format for sending
     });
+    function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
 
     const data = await response.json(); // Parse the response as JSON
     console.log('Quote posted successfully:', data); // Log response from the server
